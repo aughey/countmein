19fd8964f43b57e4c60d8d6245adb5ab3299b5b6
John Aughey <jha@aughey.com>
2011-04-09 09:57:00 -0500

    adding rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Countmein::Application.config.secret_token = 'c8e21273f37d697f50b17406c832a4b33e77b8b8bb27899e13b66e05e1aba507973a63753db939e67f1f45c8518e5d6e49f3a0e16e337712dccbe6b71f567060'
