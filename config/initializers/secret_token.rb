ca717aeb24ef253f6ab4b471ad1da39588bbebb8
John Aughey <jha@aughey.com>
2011-04-09 09:55:06 -0500

    initiazing rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Countmein::Application.config.secret_token = '5df680d8462277516613360bfd4177013169ebf4a4614160f8de0f84aea508ffb78cb4c6c60df52802f9fb62174cf9ff2642c4da01b32a9f8e1768ef7cf0ea61'
